66a478e5652386e251f81dd155d7b3d0f489506d
Aog_Vkt <65923431+vadilkt@users.noreply.github.com>
2025-10-08 16:17:38 +0100

    Feat(GEN-004): Get a profile and search a profile (#10)
    Co-authored-by: Vadil Kwekam <vadil_kwekam@Pkf-researchcenter.com>

--- src/main/java/com/example/genealogie/service/ProfileService.java
+++ src/main/java/com/example/genealogie/service/ProfileService.java
@@ -3,6 +3,10 @@ package com.example.genealogie.service;
 
 import com.example.genealogie.model.Profile;
 
+import java.util.List;
+
 public interface ProfileService {
     Profile create(Profile profile);
+    Profile getProfileById(Long id);
+    List<Profile> searchProfile(String keyword);
 }
