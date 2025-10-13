cae635ade926d8f512297ba46d0ed8219a051a53
Vadil Kwekam <vadil_kwekam@Pkf-researchcenter.com>
2025-10-13 10:27:26 +0100

    feat(GEN-009): view a professional profile

--- src/main/java/com/example/genealogie/service/ProfessionalProfileService.java
+++ src/main/java/com/example/genealogie/service/ProfessionalProfileService.java
@@ -1,8 +1,9 @@
 package com.example.genealogie.service;
 
 import com.example.genealogie.model.ProfessionalProfile;
+import com.example.genealogie.model.User;
 
 public interface ProfessionalProfileService {
-    ProfessionalProfile getById(Long id);
-    ProfessionalProfile create(ProfessionalProfile professionalProfile);
+    ProfessionalProfile getById(Long id, User user);
+    ProfessionalProfile create(ProfessionalProfile professionalProfile, User user);
 }
