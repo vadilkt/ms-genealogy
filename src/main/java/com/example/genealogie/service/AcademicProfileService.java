83935c8804d0f68ac3ea8d901685311316dca21c
Aog_Vkt <65923431+vadilkt@users.noreply.github.com>
2025-10-15 09:57:53 +0100

    feat(GEN-014): update and get an academic profile (#28)
    Co-authored-by: Vadil Kwekam <vadil_kwekam@Pkf-researchcenter.com>

--- src/main/java/com/example/genealogie/service/AcademicProfileService.java
+++ src/main/java/com/example/genealogie/service/AcademicProfileService.java
@@ -5,4 +5,6 @@ import com.example.genealogie.model.User;
 
 public interface AcademicProfileService {
     AcademicProfile create(AcademicProfile academicProfile, User user);
+    AcademicProfile update(AcademicProfile academicProfile, User user);
+    AcademicProfile getById(Long id, User user);
 }
