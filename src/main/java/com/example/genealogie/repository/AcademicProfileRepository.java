ffcf2100a456e984ee521bdfbb305351ebae14de
Aog_Vkt <65923431+vadilkt@users.noreply.github.com>
2025-10-15 14:26:51 +0100

    feat(GEN-015): get academic profiles by profile id (#29)
    Co-authored-by: Vadil Kwekam <vadil_kwekam@Pkf-researchcenter.com>

--- src/main/java/com/example/genealogie/repository/AcademicProfileRepository.java
+++ src/main/java/com/example/genealogie/repository/AcademicProfileRepository.java
@@ -1,7 +1,11 @@
 package com.example.genealogie.repository;
 
 import com.example.genealogie.model.AcademicProfile;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AcademicProfileRepository extends JpaRepository<AcademicProfile, Long> {
+    List<AcademicProfile> findAllByProfile_Id(Long profileId, Sort sort);
 }
